7f7fb473c236eb17930718984de1ba7af02517bd
root <root@dg-centos7-postgres.dg.lab.camcom.com>
2018-05-07 14:29:30 -0400

    add copyright headers

--- extinfo.sh
+++ extinfo.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# a very simple example for Starfish CLI to summarize by file type
+#
 #********************************************************
 #
 # Starfish Storage Corporation ("COMPANY") CONFIDENTIAL
@@ -29,50 +32,16 @@
 #
 #********************************************************
 
-gawk -F, '
+sf query -H -ext jpg -ext bmp -ext tiff -ext png --format "volume ext size " Target: | awk '
 BEGIN {
-	# gawk 4 feature - sort by array values descending
-	PROCINFO["sorted_in"] = "@val_num_desc"
-}
-NR > 1 {
-	dupcnt[$4]++
-	dupsize[$4] = $3; 
-	totalsize += $3
-	n = split(tolower($2), basename, "/")
-	o = split(basename[n], extarr, ".")
-	if (o > 1) {
-		ext = extarr[o]
-		# print "ext of", basename[n], "is", ext
-		extensioncnt[ext] ++
-		extensionsize[ext] += $3
-		extnum[ext] ++
+	printf("%-4s %8s %8s %8s\n", "ext", "Count", "KBused", "KBavg");
+}  
+{	extcount[tolower($2)]++; 
+	extsize[tolower($2)]+=$3
+}; 
+END { 
+	for (exttype in extcount) { 
+		printf ("%-4s %8d %8d %8.0f\n", exttype, extcount[exttype], extsize[exttype] / 1000, extsize[exttype] / extcount[exttype] / 1000 );
 	}
-} 
-END {
-	print length(dupcnt), "dups of", NR, "entries"; 
-	for (c in dupcnt) sumdupsize += dupsize[c]; 
-	print totalsize / 1000/1000/1000 ,"GB of totalspace consumed by all copies"; print (sumdupsize) / 1000/1000/1000, "GB of unique space"
-	printf("%.1f%% overhead\n",  (totalsize / sumdupsize * 100.0) - 100.0)
-	print "-----"
-	print "Top extensions by cnt"
-	i = 0
-	for (k in extensioncnt) {
-		printf("%-8s %8d   ", k, extensioncnt[k]) 
-		i++
-		if (i % 4 == 0) printf("\n")
-	}
-	printf("\n")
-	print "Top extensions by size (GB)"
-	i = 0
-	printf("%-8s %7s %7s  ", "Extname", "GBtotal", "AvgSzMB");
-	printf("%-8s %7s %7s  ", "Extname", "GBtotal", "AvgSzMB");
-	printf("%-8s %7s %7s\n", "Extname", "GBtotal", "AvgSzMB");
-	for (k in extensionsize) {
-		printf("%-8s %7.1f %7.0f  ", k, extensionsize[k]/ 1000.0/1000.0/1000.0, extensionsize[k] / extnum[k] / 1000 / 1000) 
-		i++
-		if (i % 3 == 0) printf("\n")
-	}
-	printf("\n")
-} 
-' $1/duplicated_files_list_*.csv
+}'
 
